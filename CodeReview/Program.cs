7d9aae370f3ae54988b57e837c5b84b0b0f61f04
Opilane TTHK <opilane@tthk.ee>
2021-09-28 09:43:49 +0300

    Switch

---
+++ CodeReview/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodeReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The program asks the user's name and greets the user by their name
+             */
+
+
+            /*Console.WriteLine("What's your name?");
+            string Name = Console.ReadLine();
+            Name = Console.WriteLine();
+            Console.ReadLine($"Hello, (name)");*/
+
+
+            Console.WriteLine("What's your name?");
+            String Name = Console.ReadLine();
+            Console.WriteLine($"Hello, " + Name);
+        }
+    }
+}
